69f9d534b4422315113dfc67028ecac84f5a384f
Nikhil Arora <narora200@gmail.com>
2018-06-24 03:53:18 +0530

    snake moving

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import Snake from './components/Snake';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Snake />
+      </div>
+    );
+  }
+}
+
+export default App;
